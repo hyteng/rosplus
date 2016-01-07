2229e8859e7e637c6f5d1bad4f3f152c888a5b59
haiyun.teng <tenghy@ihep.ac.cn>
2016-01-06 22:14:57 -0500

    develop 20160107v2

--- universeII/test/mqdctool.cpp
+++ universeII/test/mqdctool.cpp
@@ -258,7 +258,7 @@ static int confDuplicMQDC32[14] = {47, 54, 55, 61, 62, 73, 74, 76, 77, 78, 79, 9
 
 static int confDefaultIdxMQDC32[confSetSize] = {/*ChTh*/1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, /*board*/35, 36, /*irq*/39, 40, 43, 44, /*blt*/46, 47, /*fifo*/53, 54, 55, 56, /*adc*/60, 61, 62, 65, 66, 67, /*delay*/68, 69, 70, 71, /*io*/72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, /*pulser*/86, 87, /*rc*//*ctra*/99, 100, /*ctrb*//*limit*/112, 113, 114, 115};
 
-static uint16_t confDefaultValueMQDC32[confSetSize] = {/*ChTh*/0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /*board*/0xFF, 0, /*irq*/7, 0x00FE, 100, 4, /*blt*/0x0002, 0x0002, /*fifo*/3, 1, 1, 0, /*adc*/0, 0, 0, 0, 0, 0, /*delay*/255, 255, 0, 0, /*io*/0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, /*pulser*/0, 32, /*rc*//*ctra*/0, 0x0001, /*ctrb*//*limit*/32, 0, 16, 0};
+static uint16_t confDefaultValueMQDC32[confSetSize] = {/*ChTh*/0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /*board*/0xFF, 0, /*irq*/7, 0x00FE, 120, 4, /*blt*/0x0002, 0x0002, /*fifo*/3, 1, 1, 0, /*adc*/0, 0, 0, 0, 0, 0, /*delay*/255, 255, 0, 0, /*io*/0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, /*pulser*/0, 32, /*rc*//*ctra*/0, 0x0001, /*ctrb*//*limit*/32, 0, 16, 0};
 
 #define regSize 102
 
@@ -471,10 +471,7 @@ int main() {
     // start adc
     vme.ww(image, adc_base+MQDC32_StartAcq_Offset, vme.swap16((uint16_t)0x0001));
     sleep(1);
-    cout << "waiting irq: " << confValue[irqLevel] << ", " << confValue[irqVector] << endl;
-    vme.waitIrq(confValue[irqLevel], confValue[irqVector]);
 
-    cout << "reading data..." << endl;
     //vme.generateVmeIrq(1, 0);
     //vme.generateVmeIrq(2, 0);
     //vme.generateVmeIrq(3, 0);
@@ -486,14 +483,18 @@ int main() {
     string cmd;
 
     while(true) {
-        while(true) {
+        cout << "waiting irq: " << confValue[irqLevel] << ", " << confValue[irqVector] << endl;
+        vme.waitIrq(confValue[irqLevel], confValue[irqVector]);
+        cout << "reading data..." << endl;
+
+        if(true) {
             vme.rw(image, adc_base+0x6030, &dataLength);
             dataLength = vme.swap16(dataLength);
             cout << "data length " << dec << dataLength << hex << endl;
-            cin.sync();
-            cin >> cmd;
-            if(cmd == "quit")
-                break;
+            //cin.sync();
+            //cin >> cmd;
+            //if(cmd == "quit")
+                //break;
         }
         /*
            offset = -1;
